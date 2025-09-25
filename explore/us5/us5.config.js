0c3ad4b6929997124e26759ec1807ea0862c6862
7-7-7-15 <packgoat77.8@outlook.com>
2025-09-25 13:57:34 -0600

    Create us5.config.js

---
+++ explore/us5/us5.config.js
@@ -0,0 +1,10 @@
+self.__us5$config = {
+    prefix: '/explore/web/',
+    bare:'/spiders/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/explore/us5/us5.handler.js',
+    bundle: '/explore/us5/us5.bundle.js',
+    config: '/explore/us5/us5.config.js',
+    sw: '/explore/us5/us5.sw.js',
+}
