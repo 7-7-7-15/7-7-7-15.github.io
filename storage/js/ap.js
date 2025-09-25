d1c773724338aae1440279e202738bd61f28f581
7-7-7-15 <packgoat77.8@outlook.com>
2025-09-25 15:21:16 -0600

    Create ap.js

---
+++ storage/js/ap.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('parts', '/storage/json/particlesjs-config.json', function() {
+    console.log('callback - parts.js config loaded');
+  });
