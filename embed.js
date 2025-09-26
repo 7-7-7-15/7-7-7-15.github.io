7869f555407666e3367a258e0767e5aa86cb5e96
7-7-7-8 alt account <logan.henderson@murtaugh.k12.id.us>
2025-09-26 16:59:05 -0600

    Delete embed.js

--- embed.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const link = new URLSearchParams(window.location.search).get('p');
-  if (link) {
-    window.location.href = `/explore/web/${__us5$config.encodeUrl(link)}`;
-  }
-});
