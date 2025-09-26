2c501a3756ca12eead18e5ece94bcc75ae4d87aa
7-7-7-8 alt account <logan.henderson@murtaugh.k12.id.us>
2025-09-26 16:51:24 -0600

    Update us5.config.js

--- explore/us5/us5.config.js
+++ explore/us5/us5.config.js
@@ -1,6 +1,6 @@
 self.__us5$config = {
     prefix: '/explore/web/',
-    bare:'wss://turbiumon.top/wisp/',
+    bare:'https://embeddr.rhw.one/embed#https://wisp.mercurywork.shop',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/explore/us5/us5.handler.js',
